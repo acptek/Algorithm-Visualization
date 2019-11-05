d7b1817c2e3df31b7d4d5556cdc761f7487a882d
acptek <762612267@qq.com>
2019-11-05 14:27:05 +0800

    index for gh-page

--- JavaScript/Dijkstra.js
+++ JavaScript/Dijkstra.js
@@ -3,14 +3,14 @@ let MAXN = 32;
 let TIMEINTERVAL = 20;
 let LINEWIDTH = 2;
 let MICROSOFTFONT = "bold 10pt Microsoft JhengHei";
-let cellWidth = 50;
+let cellWidth = 30;
 let canvasWidth;
 let canvasHeight;
 let dist = new Array(); //
 let vis = new Array();
 let path = new Array();
 let primGraph = new Graph(); //
-let pathTable = new Table();
+let graphTable = new Table(); // update with primGraph.graph
 let distTable = new Table(); // update with dist
 let visTable = new Table(); // update with vis
 
@@ -31,30 +31,20 @@ function drawPath(ctx){
     }
 }
 
-const drawFinish = (String)=>{
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            DrawLayerWindow(String,
-                $("#myCanvas").position().top + BASEWIDTH,
-                $("#myCanvas").position().left + BASEWIDTH, 1500, 1, 0, 0);
-            resolve(String);
-        }, 10*TIMEINTERVAL);
-    })
-};
-
 function drawAll(ctx) {
     clearAll(ctx);
     primGraph.DrawGraph(ctx, "red", 3, "#B0C4DE", 3);
     drawPath(ctx);
     for(let i = 0; i < primGraph.vertexNum; i++){
-        ctx.fillText("Path", 0, 20);
-        ctx.fillText("Distance", 0, 3*cellWidth+10);
-        ctx.fillText("Visited", 0, 6*cellWidth+10);
+        ctx.fillText("Graph", 0, 20);
+        ctx.fillText("Distance", 0, (2+primGraph.vertexNum)*cellWidth+10);
+        ctx.fillText("Visited", 0, (5+primGraph.vertexNum)*cellWidth+10);
+        ctx.fillText(i, 20+(i+0.5)*cellWidth-5, 20+15+(2+primGraph.vertexNum)*cellWidth);
+        ctx.fillText(i, 20+(i+0.5)*50-5, 20+15+(5+primGraph.vertexNum)*cellWidth);
         ctx.fillText(i, 20+(i+0.5)*cellWidth-5, 20+15);
-        ctx.fillText(i, 20+(i+0.5)*cellWidth-5, 20+15+3*cellWidth);
-        ctx.fillText(i, 20+(i+0.5)*50-5, 20+15+6*cellWidth);
+        ctx.fillText(i, 10, 20+20+(i+0.5)*cellWidth+5);
     }
-    pathTable.DrawTable(ctx, "black", 2, "black", MICROSOFTFONT);
+    graphTable.DrawTable(ctx, "black", 2, "black", MICROSOFTFONT);
     distTable.DrawTable(ctx, "black", 2, "black", MICROSOFTFONT);
     visTable.DrawTable(ctx, "black", 2, "black", MICROSOFTFONT);
 }
@@ -87,6 +77,18 @@ function checkConnected(g){
     return true;
 }
 
+findEdgeInMatrix = function(ctx, i, j){
+    return new Promise((resolve, reject) => {
+        console.log(i, j);
+        graphTable.DrawTableElement(ctx, i, j, "yellow", 3, "black", MICROSOFTFONT);
+        graphTable.DrawTableElement(ctx, j, i, "yellow", 3, "black", MICROSOFTFONT);
+        setTimeout(()=>{
+            drawAll(ctx);
+            resolve("MatrixFind");
+        }, 30*TIMEINTERVAL);
+    })
+};
+
 findInDistance = function(ctx, j){
     return new Promise((resolve, reject) => {
         drawAll(ctx);
@@ -118,7 +120,7 @@ drawNode = function(ctx, i){
         setTimeout(()=>{
             drawAll(ctx);
             resolve("drawNode");
-        }, 50*TIMEINTERVAL);
+        }, 30*TIMEINTERVAL);
     })
 };
 
@@ -183,7 +185,6 @@ moveIndexToDist = function(ctx, pos, i){
         dist[i] = primGraph.graph[pos][i];
         distTable.body[0][i] = primGraph.graph[pos][i];
         path[i] = pos;
-        pathTable.body[0][i] = pos;
         distTable.DrawTableElement(ctx, 0, pos, "green", 3, "red", MICROSOFTFONT);
         visTable.DrawTableElement(ctx, 0, pos, "green", 3, "red", MICROSOFTFONT);
         setTimeout(()=>{
@@ -198,83 +199,38 @@ moveIndexToDist = function(ctx, pos, i){
 
 // update dist[i]
 updateDist = async function(ctx, pos){
-    return new Promise(async (resolve, reject) => {
-        drawAll(ctx);
-        for(let i = 0; i < primGraph.vertexNum; i++) {
-            await findInVisited(ctx, i);
-            if (!vis[i] && primGraph.graph[pos][i] < dist[i]) {
-                //await compareTwoPoint(ctx, pos, i, j);
-                await moveIndexToDist(ctx, pos, i); //realize dist[i] = pos
-            }
+    drawAll(ctx);
+    for(let i = 0; i < primGraph.vertexNum; i++) {
+        await findInVisited(ctx, i);
+        if (!vis[i] && primGraph.graph[pos][i] < dist[i]) {
+            //await compareTwoPoint(ctx, pos, i, j);
+            await moveIndexToDist(ctx, pos, i); //realize dist[i] = pos
         }
-        resolve("updateDist");
-    });
+    }
 };
 
-var prim_g;
+Prim = function * (ctx) {
 
-PrimRun = async (Prim_gen, String)=>{
-    let canvas = document.getElementById("myCanvas");
-    let ctx = canvas.getContext("2d");
-    Prim1(ctx);
-    // function run(Prim_gen){
-    //     prim_g = Prim_gen(ctx);
-    //     function next(data){
-    //         var result = prim_g.next(data);
-    //         if (result.done){
-    //             drawFinish(String);
-    //             return result.value;
-    //         }
-    //         if ($("#start_pause").val() == "Start"){
-    //             return result.value;
-    //         }
-    //         result.value.then(function(data){
-    //             next(data);
-    //         });
-    //     }
-    //     next();
-    // }
-    // run(Prim_gen);
-};
-
-function OneCirculation(ctx){
-    return new Promise((resolve, reject) => {
-        let t = new Text();
-        t.Inite(20, canvasHeight-20, "orange", MICROSOFTFONT, "One Circulation Finish !");
-        drawAll(ctx);
-        t.DrawText(ctx);
-        setTimeout(()=>{
-            resolve("onecirculation");
-        }, 50*TIMEINTERVAL);
-    })
-}
-
-function pause(){
-    return new Promise((resolve, reject) => {
-        resolve("pause");
-    })
-}
-
-Prim = async function * (ctx) {
     dist[primGraph.st] = 0;
     distTable.body[0][primGraph.st] = 0;
-    path[primGraph.st] = primGraph.st;
-    pathTable.body[0][primGraph.st] = primGraph.st;
+    let flag = 0;
 
-    for(let k = 0; k <= primGraph.vertexNum; k++){
-        let pos;
-        pos = await findPos(ctx);
-        yield pause();
+    for(let k = 0; k < primGraph.vertexNum; k++){
+        let pos = -1;
+        if(!flag){
+            pos = primGraph.st;
+            flag = 1;
+        }else{
+            yield pos = findPos(ctx);
+        }
         if(pos != -1){
             vis[pos] = true;
             visTable.body[0][pos] = true;
-            await updateDist(ctx, pos);
-            yield pause();
         }else{
-            console.log("fail");
+            vis[primGraph.st] = true;
+            visTable.body[0][primGraph.st] = true;
         }
-        await OneCirculation(ctx);
-        yield pause();
+        yield updateDist(ctx, pos);
     }
 };
 
@@ -282,11 +238,12 @@ Prim1 = async function(ctx) {
     dist[primGraph.st] = 0;
     distTable.body[0][primGraph.st] = 0;
     path[primGraph.st] = primGraph.st;
-    pathTable.body[0][primGraph.st] = primGraph.st;
+
     // at most n times
-    for(let k = 0; k <= primGraph.vertexNum; k++){
+    for(let k = 0; k < primGraph.vertexNum; k++){
         let pos;
         pos = await findPos(ctx);
+        console.log("Pos-->", pos);
         if(pos != -1){
             vis[pos] = true;
             visTable.body[0][pos] = true;
@@ -294,23 +251,11 @@ Prim1 = async function(ctx) {
         }else{
             console.log("fail");
         }
-        await OneCirculation(ctx);
     }
-    await drawFinish("Prim Finish !");
 };
 
 
 ///////////////////////////////////Click Events/////////////////////////////////////
-
-Start_Pause = ()=>{
-    if($("#start_pause").val() == "Pause"){
-        $("#start_pause").val("Start");
-    }else{
-        $("#next").attr("disabled", true);
-        $("#start_pause").val("Pause");
-        PrimRun(Prim, "Prim Finish !");
-    }
-};
 
 function changeSpeed() {
     var value = $('#range_speed').val();
@@ -333,26 +278,26 @@ initCanvas = function () {
         if(checkConnected(primGraph))
             break;
     }
-    pathTable.Inite(20, 20+20, 1, primGraph.vertexNum, cellWidth, cellWidth);
-    for(let i = 0; i < primGraph.vertexNum; i++){
-        path[i] = null;
-        pathTable.body[0][i] = null;
-    }
-    distTable.Inite(20, 20+20+3*cellWidth, 1, primGraph.vertexNum, cellWidth, cellWidth);
+    graphTable.Inite(20, 20+20, primGraph.vertexNum, primGraph.vertexNum, cellWidth, cellWidth);
+    graphTable.body = primGraph.graph;
+    distTable.Inite(20, 20+20+primGraph.vertexNum*cellWidth+2*cellWidth, 1, primGraph.vertexNum, cellWidth, cellWidth);
     for(let i = 0; i < primGraph.vertexNum; i++){
         dist[i] = INF;
         distTable.body[0][i] = dist[i];
     }
-    visTable.Inite(20, 20+20+6*cellWidth, 1, primGraph.vertexNum, cellWidth, 50);
+    visTable.Inite(20, 20+20+primGraph.vertexNum*cellWidth+5*cellWidth, 1, primGraph.vertexNum, cellWidth, 50);
     for(let i = 0; i < primGraph.vertexNum; i++){
         vis[i] = false;
         visTable.body[0][i] = vis[i];
+    }
+    for(let i = 0; i < primGraph.vertexNum; i++){
+        path[i] = null;
     }
     drawAll(ctx);
     primGraph.st = 0;
     //drawNode(ctx, 3);
     //findInVisited(ctx, 3);
-    //Prim1(ctx);
+    Prim1(ctx);
     //findEdgeInMatrix(ctx, 1, 3);
 };
 
